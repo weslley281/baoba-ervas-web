2de82dfe84cd6ca6696e94813ee8111ee3850c47
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-12-29 17:40:53 -0400

    a

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,32 +1,38 @@
 import { SignOut, House } from 'phosphor-react';
 import { Container } from './styles';
+import logoPNG from '../../assets/logo.png';
 
 export function Menu() {
   return (
     <Container>
       <div className="topnav" id="myTopnav">
-        <a href="/" className="active home">
-          <House size={24} />
-          Home
-        </a>
-        <a href="/">Produtos</a>
-
-        <div className="dropdown">
-          <button className="dropbtn">
-            Categorias
-            <i className="fa fa-caret-down"></i>
-          </button>
-          <div className="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
+        <div className="part1">
+          <img src={logoPNG} alt="" />
         </div>
-        <a href="#about">Sobre</a>
-        <a href="#contact">Contato</a>
-        <a href="#" className="logOut">
-          <SignOut size={24} />
-        </a>
+        <div className="part2">
+          <a href="/" className="active home">
+            <House size={24} />
+            Home
+          </a>
+          <a href="/">Produtos</a>
+
+          <div className="dropdown">
+            <button className="dropbtn">
+              Categorias
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div>
+          <a href="#about">Sobre</a>
+          <a href="#contact">Contato</a>
+          <a href="#" className="logOut">
+            <SignOut size={24} />
+          </a>
+        </div>
       </div>
     </Container>
   );
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -6,81 +6,116 @@ export const Container = styled.nav`
   flex: 1;
 
   .topnav {
+    display: flex;
+    flex: 1;
     overflow: hidden;
+    justify-content: space-between;
 
-    a {
-      float: left;
-      display: block;
-      color: ${({ theme }) => theme.white};
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-      font-size: 17px;
-
-      &:hover,
-      .dropdown:hover .dropbtn {
-        background-color: ${({ theme }) => theme.secondary};
-        color: white;
+    .part1 {
+      img {
+        width: 250px;
+        height: 70px;
         border-radius: 6px;
+        padding: 2px 0 0 5px;
       }
     }
 
-    .active {
-      background-color: ${({ theme }) => theme.main};
-      color: white;
-    }
-
-    .icon {
-      display: none;
-    }
-
-    .dropdown {
-      float: left;
-      overflow: hidden;
-
-      .dropbtn {
-        font-size: 17px;
-        border: none;
-        outline: none;
-        color: white;
+    .part2 {
+      a {
+        float: left;
+        display: block;
+        color: ${({ theme }) => theme.white};
+        text-align: center;
         padding: 14px 16px;
-        background-color: inherit;
-        font-family: inherit;
-        margin: 0;
+        text-decoration: none;
+        font-size: 17px;
+
+        &:hover,
+        .dropdown:hover .dropbtn {
+          background-color: ${({ theme }) => theme.secondary};
+          color: white;
+          border-radius: 6px;
+        }
       }
 
-      .dropdown-content {
+      .active {
+        background-color: ${({ theme }) => theme.main};
+        color: white;
+      }
+
+      .icon {
         display: none;
-        position: absolute;
-        background-color: ${({ theme }) => theme.white};
-        min-width: 160px;
-        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-        z-index: 1;
-        border-radius: 6px;
+      }
 
-        a {
-          float: none;
-          color: black;
-          padding: 12px 16px;
-          text-decoration: none;
-          display: block;
-          text-align: left;
+      .dropdown {
+        float: left;
+        overflow: hidden;
 
-          &:hover {
-            background-color: ${({ theme }) => theme.secondary};
+        .dropbtn {
+          font-size: 17px;
+          border: none;
+          outline: none;
+          color: white;
+          padding: 14px 16px;
+          background-color: inherit;
+          font-family: inherit;
+          margin: 0;
+        }
+
+        .dropdown-content {
+          display: none;
+          position: absolute;
+          background-color: ${({ theme }) => theme.white};
+          min-width: 160px;
+          box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+          z-index: 1;
+          border-radius: 6px;
+
+          a {
+            float: none;
             color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.secondary};
+              color: black;
+            }
           }
         }
       }
-    }
-    .dropdown:hover .dropdown-content {
-      display: block;
-    }
+      .dropdown:hover .dropdown-content {
+        display: block;
+      }
 
-    .home {
-      display: flex;
+      .home {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+  @media only screen and (max-width: 600px) {
+    .topnav {
+      display: block;
+      flex: 1;
+      overflow: hidden;
       align-items: center;
       justify-content: center;
+
+      .part1 {
+        padding: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .part2 {
+        display: flex;
+        align-items: center;
+      }
     }
   }
 `;
