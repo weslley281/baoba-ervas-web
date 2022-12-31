46cd194e5f32f8beeb00d43303250bda6c786249
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-12-31 10:18:38 -0400

    a

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const url = 'http://192.168.15.41:5000'
-const url = 'http://192.168.1.11:5000';
+const url = 'http://192.168.15.41:5000';
+// const url = 'http://192.168.1.11:5000';
 
 export const api = axios.create({ baseURL: url });
